343f2f3d77cc1b1c813885fe74f1462433d702f2
Dongjin Li <liyangxiaobei@gmail.com>
2026-06-09 02:30:09 +0000

    version 4.1.0

--- R/svenetics.R
+++ R/svenetics.R
@@ -4,10 +4,8 @@
 svenetics <- function() {
   app_dir <- system.file("app", package = "bravo")
   if (app_dir == "") {
-    stop("Could not find the app directory. Please try re-installing bravo.")
+    stop("Could not find the app directory. Please try re-installing svenetics.")
   }
   runApp(app_dir, display.mode = "normal")
 }
-
-
 
